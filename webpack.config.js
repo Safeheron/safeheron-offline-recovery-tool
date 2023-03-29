83fc678d0ea296b629848fe1f4eb1fa5db52c51c
ty <yangtianyi@safeheron.com>
2023-03-29 17:40:08 +0800

    fix: macos compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(@tauri-apps\/))/,
         use: 'babel-loader',
       },
       {
