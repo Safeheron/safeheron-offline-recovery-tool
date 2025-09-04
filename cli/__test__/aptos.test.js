879669d28a1d7936587463af0bb511629a71319f
ty <786220806@qq.com>
2025-09-04 16:08:29 +0800

    feat: add aptos fa token cli transfer

--- cli/__test__/aptos.test.js
+++ cli/__test__/aptos.test.js
@@ -7,19 +7,29 @@ import { transfer, ftTransfer } from '../aptos'
 
 test('transfer apt', async () => {
   await expect(transfer({
-    receiver: '0x4a1bdbd47754cef11c46aceb1a52d096e9b6d9cf3ef8f7075d8f134608217550',
+    receiver: '0x2dad9195a194e202ece81764b933c2769004e5ddc864dc9c3e323aa310e5e850',
     amount: '0.00001',
     privateKey: 'aac52ab8254151d0913ea5fe18c0f0a4b5196e9fad6a3c55d3feea268cd7cb06',
     network: 'testnet',
   })).resolves.not.toThrow()
 })
 
-test('transfer USDT token', async () => {
+test('transfer apt by ftTransfer', async () => {
   await expect(ftTransfer({
-    receiver: '0x7921a7993ed2b9ed37359cb11e74af7e9422ffb2b2b580dc07e840c5578f3924',
+    receiver: '0x2dad9195a194e202ece81764b933c2769004e5ddc864dc9c3e323aa310e5e850',
     amount: '0.00001',
     privateKey: 'aac52ab8254151d0913ea5fe18c0f0a4b5196e9fad6a3c55d3feea268cd7cb06',
     network: 'testnet',
-    ftoken: '0x43417434fd869edee76cca2a4d2301e528a1551b1d719b75c350c3c97d15b8b9::coins::USDT',
-  })).rejects.toThrow('not support')
+    ftoken: '0x1::aptos_coin::AptosCoin',
+  })).resolves.not.toThrow()
+})
+
+test('transfer test token', async () => {
+  await expect(ftTransfer({
+    receiver: '0x2dad9195a194e202ece81764b933c2769004e5ddc864dc9c3e323aa310e5e850',
+    amount: '0.00001',
+    privateKey: '8393a847964461ffd5151f57fc2be3f2e95200760b523413f011cb3e34af770e',
+    network: 'testnet',
+    ftoken: '0x5f4ca7dc97481e48a9d84f109c1e5532d8961735809821046fc5fa27ff351298',
+  })).resolves.not.toThrow()
 })
